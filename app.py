927f2ffcddfcbc532831461acc4fb21ac98915a8
Malyshev Valery <malyshevalery@gmail.com>
2021-02-26 10:46:16 +0300

    Google login and refactor

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 """Main code of the app"""
-from typing import Optional
 import json
-from flask import Flask, render_template, Response, request
+from typing import Optional
+
 import requests
+from flask import Flask, render_template, Response, request
 from flask_material import Material
 from werkzeug.utils import secure_filename
 
-from config import LOGIN_TYPES
+from config import LOGIN_TYPES, URL
 from secrets import SECRETS
 
 app = Flask(__name__)
@@ -38,8 +39,14 @@ def page():
 @app.route('/login/<login_type>', methods=['GET'])
 def login(login_type: str):
     """Redirect to login screen (not yet implemented)"""
+    base_link = LOGIN_TYPES[login_type]
+    config = SECRETS[login_type]
+    link = f'{base_link}?client_id={config.id}'
+    link += f'&response_type=code&redirect_uri={URL}/profile/{login_type}'
+    if config.scope is not None:
+        link += f'&scope={config.scope}'
     return render_template('login.html', login_type=login_type,
-                           login_link=LOGIN_TYPES[login_type])
+                           login_link=link)
 
 
 @app.route('/profile/<login_type>', methods=['GET'])
@@ -47,24 +54,27 @@ def profile(login_type: str):
     """Profile page of a user"""
     code = request.args.get('code')
     secret = SECRETS[login_type]
+    print(f'{URL}/profile/{login_type}')
     r = requests.post(secret.access_url, data={
         'client_id': secret.id,
         'client_secret': secret.secret,
-        'code': code
+        'code': code,
+        'redirect_uri': f'{URL}/profile/{login_type}',
+        'grant_type': 'authorization_code',
     }, headers={'Accept': 'application/json'})
     res = json.loads(r.text)
     if 'error' in res:
         return render_template('error.html', error=res['error'])
     else:
         access_token = res['access_token']
-        r = requests.get('https://api.github.com/user', headers={
-            'Authorization': f'token {access_token}',
+        r = requests.get(secret.info_url, headers={
+            'Authorization': secret.header % access_token,
             'Accept': 'application/json',
         })
         res = json.loads(r.text)
         print(res)
         return render_template('profile.html', type=login_type,
-                               name=res['login'])
+                               name=secret.get_name(res))
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 """Configuration details"""
 
+URL = 'http://localhost:5000'
 LOGIN_TYPES = {
     'imagelab': 'https://some_login.com',
-    'github': 'https://github.com/login/oauth/authorize?client_id=88c9085f3c5fa7c1942e'
+    'github': 'https://github.com/login/oauth/authorize',
+    'google': 'https://accounts.google.com/o/oauth2/v2/auth'
 }
